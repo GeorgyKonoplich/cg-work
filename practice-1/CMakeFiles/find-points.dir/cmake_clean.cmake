454256636b1d24b07a4c4e3fb7c83fc849f31373
j0r1k <j0r1k@j0r1k.(none)>
2013-04-08 02:12:45 +0400

    version_4

---
+++ practice-1/CMakeFiles/find-points.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/find-points.dir/find-points.cpp.o"
+  "find-points.pdb"
+  "find-points"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/find-points.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
